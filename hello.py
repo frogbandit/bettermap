bd7e429dbc2057b7cc8c70f8f89cc16b1bcd74b4
frogbandit <jx2218@columbia.edu>
2014-07-03 14:42:53 +0800

    make it work hello world

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
-from sqlalchemy import *
-import simplejson as json
-import collections
-import requests
+# from flask import Flask, render_template, request
+# from sqlalchemy import *
+# import simplejson as json
+# import collections
+# import requests
 
-import os 
-import psycopg2
-import urlparse
+# import os 
+# import psycopg2
+# import urlparse
 
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -22,71 +24,72 @@ app = Flask(__name__)
 
 # @app.route("/search", methods=["GET", "POST"])
 def hello():
+	return "hello world"
 
-	db = create_engine('postgresql://postgres:cloudminer@localhost:5432/postgres')
-	metadata = MetaData(db)
-	users = Table('map_resources_table', metadata, autoload=True)
-	#users = Table('map_user_projects', metadata, autoload=True)
-	#s = users.select(and_(not_(users.c.latitude == 0))).order_by("longitude DESC")
-	#users.c.id != 114806, users.c.id!= 114716, users.c.id != 114859
-	s = users.select(not_(users.c.latitude == 0))
+	# db = create_engine('postgresql://postgres:cloudminer@localhost:5432/postgres')
+	# metadata = MetaData(db)
+	# users = Table('map_resources_table', metadata, autoload=True)
+	# #users = Table('map_user_projects', metadata, autoload=True)
+	# #s = users.select(and_(not_(users.c.latitude == 0))).order_by("longitude DESC")
+	# #users.c.id != 114806, users.c.id!= 114716, users.c.id != 114859
+	# s = users.select(not_(users.c.latitude == 0))
 
-	rs = s.execute()
-	rows = rs.fetchmany(100000)
-	#196880
-	#Moghoweyik River
-	#-171.6199
+	# rs = s.execute()
+	# rows = rs.fetchmany(100000)
+	# #196880
+	# #Moghoweyik River
+	# #-171.6199
 
-	rowarray_list = []
+	# rowarray_list = []
 
-	#filename = request.form["commodity"]
-	count = 0
-	for row in rows:
+	# #filename = request.form["commodity"]
+	# count = 0
+	# for row in rows:
 
-		# if filename in row1.commodity_name:
-			geometry = {'coordinates': [row.longitude, row.latitude], 'type': "Point"}
-			# properties = {'publicid': row.project_name, 'origintime': row.project_name, 'longitude': row.longitude, 'latitude': row.latitude, 'depth': row.id, 
-			# 'magnitude': row.id, 'magnitudetype': row.project_name, 'status': row.project_name, 'phases': row.id, 'type': row.project_name, 
-			# 'agency': row.project_name, 'updatetime': row.project_name, 'bbox': [row.longitude, row.latitude, row.longitude, row.latitude]}
-			properties = {}
-			t = {'type': "Feature", 'geometry': geometry, 'geometry_name': row.project_name, 'properties': properties, 'commodity': row.commodity_name, 'country_code': row.country_code}
-		# for row2 in rows:
+	# 	# if filename in row1.commodity_name:
+	# 		geometry = {'coordinates': [row.longitude, row.latitude], 'type': "Point"}
+	# 		# properties = {'publicid': row.project_name, 'origintime': row.project_name, 'longitude': row.longitude, 'latitude': row.latitude, 'depth': row.id, 
+	# 		# 'magnitude': row.id, 'magnitudetype': row.project_name, 'status': row.project_name, 'phases': row.id, 'type': row.project_name, 
+	# 		# 'agency': row.project_name, 'updatetime': row.project_name, 'bbox': [row.longitude, row.latitude, row.longitude, row.latitude]}
+	# 		properties = {}
+	# 		t = {'type': "Feature", 'geometry': geometry, 'geometry_name': row.project_name, 'properties': properties, 'commodity': row.commodity_name, 'country_code': row.country_code}
+	# 	# for row2 in rows:
 
-		# 	if (row1.latitude == row2.latitude) and (row1.longitude == row2.longitude):
-		# 		count = count + 1
+	# 	# 	if (row1.latitude == row2.latitude) and (row1.longitude == row2.longitude):
+	# 	# 		count = count + 1
 
-		# t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
-			rowarray_list.append(t)
-			count = 0
-
-
-			# if filename in row1.commodity_name:
-		# 	point = {'coordinates': [row.latitude, row.longitude]}
-		# 	t = {'point': point, 'project': row.project_name}
-		# # for row2 in rows:
-
-		# # 	if (row1.latitude == row2.latitude) and (row1.longitude == row2.longitude):
-		# # 		count = count + 1
-
-		# # t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
-		# 	rowarray_list.append(t)
-		# 	count = 0
+	# 	# t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
+	# 		rowarray_list.append(t)
+	# 		count = 0
 
 
+	# 		# if filename in row1.commodity_name:
+	# 	# 	point = {'coordinates': [row.latitude, row.longitude]}
+	# 	# 	t = {'point': point, 'project': row.project_name}
+	# 	# # for row2 in rows:
 
-   	j = json.dumps(rowarray_list,  use_decimal=True, sort_keys = True)
+	# 	# # 	if (row1.latitude == row2.latitude) and (row1.longitude == row2.longitude):
+	# 	# # 		count = count + 1
 
-   	j = '{"type": "FeatureCollection", "features":' + j + ', "crs":{"type":"EPSG","properties":{"code":"4326"}},"bbox":[-1000, 1000, -1000, -1000]}'
-   	print j[-1000:-1]
-   	#j = '{"type": "", "features":' + j + ', "crs": {}, "bbox": []}'
-   	#j = '{"objects":' + j + '}'
-   	f = open('static/data/data.json', 'w+')
-   	f.write(j)
-   	f.close()
+	# 	# # t = {'lon': row1.longitude, 'lat': row1.latitude, 'project': row1.project_name, 'number': row1.id}
+	# 	# 	rowarray_list.append(t)
+	# 	# 	count = 0
 
 
 
-	return render_template("earthquake.html")
+ #   	j = json.dumps(rowarray_list,  use_decimal=True, sort_keys = True)
+
+ #   	j = '{"type": "FeatureCollection", "features":' + j + ', "crs":{"type":"EPSG","properties":{"code":"4326"}},"bbox":[-1000, 1000, -1000, -1000]}'
+ #   	print j[-1000:-1]
+ #   	#j = '{"type": "", "features":' + j + ', "crs": {}, "bbox": []}'
+ #   	#j = '{"objects":' + j + '}'
+ #   	f = open('static/data/data.json', 'w+')
+ #   	f.write(j)
+ #   	f.close()
+
+
+
+	# return render_template("earthquake.html")
 
 
 # if __name__ == "__main__":
